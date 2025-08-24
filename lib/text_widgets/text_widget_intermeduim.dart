8c163f4dcd721283049dcadc52835d7d1af6daac
L0opMaster <sothy1223@gmail.com>
2025-08-24 16:34:32 +0700

    My first

--- lib/text_widgets/text_widget_intermeduim.dart
+++ lib/text_widgets/text_widget_intermeduim.dart
@@ -12,6 +12,72 @@ class TextWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TextWidgetIntermediate"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(
+              textAlign: TextAlign.justify,
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFFde7607),
+                letterSpacing: 1.2,
+                height: 1.5,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.black,
+                decorationStyle: TextDecorationStyle.solid,
+              ),
+            ),
+            Text(
+              textAlign: TextAlign.start,
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFFde7607),
+                letterSpacing: 1.2,
+                height: 1.5,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.black,
+                decorationStyle: TextDecorationStyle.solid,
+              ),
+            ),
+            Text(
+              textAlign: TextAlign.end,
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFFde7607),
+                letterSpacing: 1.2,
+                height: 1.5,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.black,
+                decorationStyle: TextDecorationStyle.solid,
+              ),
+            ),
+            Text(
+              textAlign: TextAlign.justify,
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFFde7607),
+                letterSpacing: 1.2,
+                height: 1.5,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.black,
+                decorationStyle: TextDecorationStyle.solid,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
